5d40c31a0bd813248e9087ef778ffe304ef3e805
sontsevolosa <sontsevolosa@gmail.com>
2016-07-01 11:22:46 +0300

    #8 Making changes in spaces

--- test/controllers/lunches_controller_test.rb
+++ test/controllers/lunches_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class LunchesControllerTest < ActionController::TestCase
   test 'should get index' do
-  	get :index
-  	assert_response :success
-  	assert_select 'title', 'Lunchez'
+    get :index
+    assert_response :success
+    assert_select 'title', 'Lunchez'
   end
 end
