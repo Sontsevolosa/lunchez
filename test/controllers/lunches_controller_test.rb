ef5ffe9e89200130cc762f1568475c93f3a1e7ed
sontsevolosa <sontsevolosa@gmail.com>
2016-07-08 17:02:05 +0300

    #4 Making changes in Lunches controller test for getting index page

--- test/controllers/lunches_controller_test.rb
+++ test/controllers/lunches_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class LunchesControllerTest < ActionController::TestCase
-  test 'should get index' do
+  test 'should get index after successful login' do
+    sign_in users(:default)
     get :index
     assert_response :success
     assert_select 'title', 'Lunchez'
