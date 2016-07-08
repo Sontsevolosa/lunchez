886ec4987814812247f42b1e46ffbce797b92bbb
sontsevolosa <sontsevolosa@gmail.com>
2016-07-08 16:37:51 +0300

    #4 Adding Devise test helpers and Warden test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,16 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+include Warden::Test::Helpers
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
