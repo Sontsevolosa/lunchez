1e082eebc5322bf703202ea94b5a2b31ca36b101
sontsevolosa <sontsevolosa@gmail.com>
2016-07-08 16:41:35 +0300

    #4 Making changes in test for layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test 'layout links' do
+    login_as users(:default)
     get root_path
     assert_template 'lunches/index'
     assert_select 'a[href=?]', root_path, count: 3
