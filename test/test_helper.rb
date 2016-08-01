9391d920c91f4280cc817111dc002a37d16462ec
sontsevolosa <sontsevolosa@gmail.com>
2016-08-01 17:47:02 +0300

    #25 Making Capybara available in all integration test cases

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,4 +18,10 @@ end
 
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
+  include Capybara::DSL
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
