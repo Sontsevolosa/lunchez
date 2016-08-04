89381e38891cdb53e0fa3549c289d1934159a6d1
sontsevolosa <sontsevolosa@gmail.com>
2016-08-04 19:39:21 +0300

    #25 Adding an integration test for checking 'Sign out' link

--- test/integration/signout_button_test.rb
+++ test/integration/signout_button_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class SignoutButtonTest < ActionDispatch::IntegrationTest
-  
+  test 'sign out button' do
+    visit('/')
+    sign_in users(:default)
+    click_button('Log in')
+    visit('/')
+    screenshot_and_save_page
+    click_on('Sign out')
+    assert page.has_content?('Log in'), 'Your page does not have content'
+  end
 end
