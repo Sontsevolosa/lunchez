f649533d98a94ffba8d20b0023260024457ab2fc
sontsevolosa <sontsevolosa@gmail.com>
2016-08-05 18:05:23 +0300

    #25 Commenting line with code for screenshot

--- test/integration/signout_button_test.rb
+++ test/integration/signout_button_test.rb
@@ -6,9 +6,9 @@ class SignoutButtonTest < ActionDispatch::IntegrationTest
     sign_in users(:default)
     click_button('Log in')
     assert_text('Hello everyone!')
-    
+
     visit root_path
-    screenshot_and_save_page
+    #screenshot_and_save_page
     click_on('Sign out')
     assert page.has_content?('Log in'), 'Your page does not have content'
   end
