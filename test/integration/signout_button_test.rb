91b465d51a36bf93241e41c4c746111eccd79b8b
sontsevolosa <sontsevolosa@gmail.com>
2016-08-05 17:53:47 +0300

    #25 Adding root path using test helper

--- test/integration/signout_button_test.rb
+++ test/integration/signout_button_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class SignoutButtonTest < ActionDispatch::IntegrationTest
   test 'sign out button' do
-    visit('/')
+    visit root_path
     sign_in users(:default)
     click_button('Log in')
-    visit('/')
+    visit root_path
     screenshot_and_save_page
     click_on('Sign out')
     assert page.has_content?('Log in'), 'Your page does not have content'
